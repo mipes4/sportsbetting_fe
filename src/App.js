18c990b0a84b5f45443e8219f0e53af29a7874c5
mipes4 <michellepesch@gmail.com>
2020-07-01 08:34:31 +0200

    Made a navigation component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
+import NavBar from "./components/navigation/NavigationComponent";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
