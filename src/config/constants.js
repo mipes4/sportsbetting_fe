00780cda0d0abe59892acfc06e325593ecad07da
mipes4 <michellepesch@gmail.com>
2020-07-12 09:48:35 +0200

    Implemented the process.env file, server is up and running

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+// export const apiUrl = "https://sports-betting-app.herokuapp.com";
+export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const apiUrlFootballDemo = "https://www.api-football.com/demo/v2";
